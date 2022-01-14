d35897537bbd96311f203e655236b4c64c15360f
secretpray <a.p.ruban@gmail.com>
2022-01-14 10:30:20 +0200

    Only registered users can vote

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import AutohideController from "./autohide_controller.js"
 application.register("autohide", AutohideController)
+
+import CheckCurrentUserController from "./check_current_user_controller.js"
+application.register("check-current-user", CheckCurrentUserController)
