5b904935920adf920595d6915b71e35246f8db25
secretpray <a.p.ruban@gmail.com>
2022-01-14 11:00:26 +0200

    Added comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,17 +1,25 @@
 class Comment < ApplicationRecord
-  # include ActionView::RecordIdentifier
   belongs_to :user
   has_rich_text :content
   has_many :votes, dependent: :destroy
 
   after_create_commit do
-    broadcast_append_to :comments,
-      target: "comments",
-      partial: "comments/comment_for_stream",
-      locals: { comment: self }
+    broadcast_append_to :comments, target: "comments", partial: "comments/comment_for_stream", locals: { comment: self }
+    update_counter
+  end
+
+  after_destroy_commit do
+    broadcast_remove_to :comments
+    update_counter
   end
 
   def vote_count
     votes.sum(:choice)
+  end
+
+  private
+
+  def update_counter
+    broadcast_update_to :comments, target: "comments_counter", partial: 'comments/count', locals: { count: Comment.count }
   end
 end
