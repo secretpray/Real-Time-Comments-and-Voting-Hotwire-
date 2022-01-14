5c66a3fff33da7253a45717a1a6f9ba76f82af23
secretpray <a.p.ruban@gmail.com>
2022-01-14 10:07:06 +0200

    Autohide Flash controller

---
+++ app/javascript/controllers/autohide_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="autohide"
+export default class extends Controller {
+  connect() {
+    setTimeout(() => this.dismiss(), 5000)
+  }
+
+  dismiss() {
+    this.element.remove()
+  }
+}
