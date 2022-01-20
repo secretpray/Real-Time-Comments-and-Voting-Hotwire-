bb35ccdfdc053b050bd4fff1e4017c8c3bcdabc0
secretpray <a.p.ruban@gmail.com>
2022-01-20 09:12:05 +0200

    Comment show page move to turbo_frame Comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     vote = comment.votes.find_by(user_id: user&.id)
     vote&.choice == action ? 'undo vote' : action
   end
+
+  def up_title(comment, static)
+    static ? 'up_vote' : set_title(comment, 'up_vote', current_user)
+  end
+
+  def down_title(comment, static)
+    static ? 'down_vote' : set_title(comment, 'down_vote', current_user)
+  end
 end
